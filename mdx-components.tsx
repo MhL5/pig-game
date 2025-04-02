975cdd1bf55daa15ab7a527b61f09b7ff58b5ce6
Mohammad Hosein Lashani <sapumr@gmail.com>
2025-04-02 17:45:56 +0330

    update mdx components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,70 +1,26 @@
-import React, { ComponentPropsWithoutRef } from 'react';
-import Link from 'next/link';
-import { highlight } from 'sugar-high';
-
-type HeadingProps = ComponentPropsWithoutRef<'h1'>;
-type ParagraphProps = ComponentPropsWithoutRef<'p'>;
-type ListProps = ComponentPropsWithoutRef<'ul'>;
-type ListItemProps = ComponentPropsWithoutRef<'li'>;
-type AnchorProps = ComponentPropsWithoutRef<'a'>;
-type BlockquoteProps = ComponentPropsWithoutRef<'blockquote'>;
+import React, { ComponentPropsWithoutRef } from "react";
+import Link from "next/link";
+import { codeToHtml } from "shiki";
 
 const components = {
-  h1: (props: HeadingProps) => (
-    <h1 className="font-medium pt-12 mb-0" {...props} />
-  ),
-  h2: (props: HeadingProps) => (
-    <h2
-      className="text-gray-800 dark:text-zinc-200 font-medium mt-8 mb-3"
-      {...props}
-    />
-  ),
-  h3: (props: HeadingProps) => (
-    <h3
-      className="text-gray-800 dark:text-zinc-200 font-medium mt-8 mb-3"
-      {...props}
-    />
-  ),
-  h4: (props: HeadingProps) => <h4 className="font-medium" {...props} />,
-  p: (props: ParagraphProps) => (
-    <p className="text-gray-800 dark:text-zinc-300 leading-snug" {...props} />
-  ),
-  ol: (props: ListProps) => (
-    <ol
-      className="text-gray-800 dark:text-zinc-300 list-decimal pl-5 space-y-2"
-      {...props}
-    />
-  ),
-  ul: (props: ListProps) => (
-    <ul
-      className="text-gray-800 dark:text-zinc-300 list-disc pl-5 space-y-1"
-      {...props}
-    />
-  ),
-  li: (props: ListItemProps) => <li className="pl-1" {...props} />,
-  em: (props: ComponentPropsWithoutRef<'em'>) => (
-    <em className="font-medium" {...props} />
-  ),
-  strong: (props: ComponentPropsWithoutRef<'strong'>) => (
-    <strong className="font-medium" {...props} />
-  ),
-  a: ({ href, children, ...props }: AnchorProps) => {
+  a: ({ href, children, ...props }: ComponentPropsWithoutRef<"a">) => {
     const className =
-      'text-blue-500 hover:text-blue-700 dark:text-gray-400 hover:dark:text-gray-300 dark:underline dark:underline-offset-2 dark:decoration-gray-800';
-    if (href?.startsWith('/')) {
+      "text-blue-500 hover:text-blue-700 dark:text-blue-700 dark:hover:text-blue-500";
+
+    if (href?.startsWith("/"))
       return (
         <Link href={href} className={className} {...props}>
           {children}
         </Link>
       );
-    }
-    if (href?.startsWith('#')) {
+
+    if (href?.startsWith("#"))
       return (
         <a href={href} className={className} {...props}>
           {children}
         </a>
       );
-    }
+
     return (
       <a
         href={href}
@@ -77,9 +33,36 @@ const components = {
       </a>
     );
   },
-  code: ({ children, ...props }: ComponentPropsWithoutRef<'code'>) => {
-    const codeHTML = highlight(children as string);
-    return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
+  code: async ({
+    children,
+    className,
+    ...props
+  }: ComponentPropsWithoutRef<"code">) => {
+    if (className?.startsWith("language-")) {
+      // If it's a code block
+      const lang = className.replace("language-", ""); // Extract the language (e.g., ts, js)
+      const codeHTML = await codeToHtml(children as string, {
+        lang,
+        theme: "github-dark",
+      });
+
+      return (
+        <code
+          dangerouslySetInnerHTML={{ __html: codeHTML }}
+          {...props}
+          className={className}
+        />
+      );
+    }
+
+    return (
+      <code
+        {...props}
+        className="prose-code:after:content-none rounded-sm bg-gray-200 px-1 py-0.5 text-gray-800"
+      >
+        {typeof children === "string" ? children.replaceAll("`", "") : children}
+      </code>
+    );
   },
   Table: ({ data }: { data: { headers: string[]; rows: string[][] } }) => (
     <table>
@@ -100,12 +83,6 @@ const components = {
         ))}
       </tbody>
     </table>
-  ),
-  blockquote: (props: BlockquoteProps) => (
-    <blockquote
-      className="ml-[0.075em] border-l-3 border-gray-300 pl-4 text-gray-700 dark:border-zinc-600 dark:text-zinc-300"
-      {...props}
-    />
   ),
 };
 
